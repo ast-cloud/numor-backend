01f5ad6c2c8525e1887f534f2693ed51b752d9a5
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2025-12-20 19:59:05 +0530

    Pushing to main

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// src/app.js
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+const errorMiddleware = require('./middlewares/error.middleware');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.use('/api', routes);
+app.use(errorMiddleware);
+
+module.exports = app;
