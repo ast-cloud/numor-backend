f7945cb9a2a4bcf6fff39cd978e9b332a2f05ead
Aastik <aastikyadav1997.ay@gmail.com>
2026-01-30 01:51:16 +0530

    Changed cookie settings

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -7,7 +7,7 @@ async function register(req, res) {
     res.cookie("access_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -34,7 +34,7 @@ async function login(req, res) {
     res.cookie("access_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict", // or "lax"
+      sameSite: "lax", // or "lax"
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
